0df070d19ed7062f238d671e1b6752ce337a76f7
steven <stev3np@gmail.com>
2025-03-24 17:36:14 -0500

    Se agrego una subclase CasaRural, atributos específicos. distanciaCabecera Distancia en km a la cabecera municipal altitud en metros sobre el nivel del mar.

---
+++ src/tipoinmueble/CasaRural.java
@@ -0,0 +1,19 @@
+package tipoinmueble;
+
+public class CasaRural extends Casa {
+    private int distanciaCabecera;
+    private int altitud;
+
+    public CasaRural(int IdInmobiliario, int area, String direccion, int habitaciones, int baños, int numeroPisos, int distanciaCabecera, int altitud) {
+        super(IdInmobiliario, area, direccion, habitaciones, baños, numeroPisos);
+        this.distanciaCabecera = distanciaCabecera;
+        this.altitud = altitud;
+    }
+
+    @Override
+    public void imprimir() {
+        super.imprimir();
+        System.out.println("Distancia a Cabecera: " + distanciaCabecera + " km");
+        System.out.println("Altitud: " + altitud + " m");
+    }
+}
